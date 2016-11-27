0a14f3f45b777f1c181e518d121b1f0d808c0e24
Michael Cheng <mcheng.oc@gmail.com>
2016-11-26 21:39:48 -0800

    Added orbit carousel

--- app/components/Slides.jsx
+++ app/components/Slides.jsx
@@ -4,7 +4,14 @@ var Slides = React.createClass({
   render: function() {
     return (
       <div>
-        <h3>Slides Component</h3>
+        <div className="orbit" role="region" aria-label="Event Pictures" data-orbit>
+          <ul className="orbit-container">
+            <li className="orbit-slide">
+              <img className="orbit-image" src="/img/corgie1.jpg" alt="Space"/>
+              <figcaption className="orbit-caption">Chompers, THe Corgie!!!.</figcaption>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
