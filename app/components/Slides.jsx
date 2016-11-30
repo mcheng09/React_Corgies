c1cd0a54bf30c3a96bdc231646174f890f82a804
Michael Cheng <mcheng.oc@gmail.com>
2016-11-30 13:36:28 -0800

    Removed carousel

--- app/components/Slides.jsx
+++ app/components/Slides.jsx
@@ -4,28 +4,7 @@ var Slides = React.createClass({
   render: function() {
     return (
       <div>
-        <div className="orbit" role="region" aria-label="Event Pictures" data-orbit>
-          <ul className="orbit-container">
-            <button className="orbit-previous"><span className="show-for-sr">Previous Slide</span> &#9664;&#xFE0E;</button>
-            <button className="orbit-next"><span className="show-for-sr">Next Slide</span> &#9654;&#xFE0E;</button>
-            <li className="is-active orbit-slide">
-              <img className="orbit-image" src="/img/corgie1.jpg" alt="Space"/>
-              <figcaption className="orbit-caption">Chompers, THe Corgie!!!.</figcaption>
-            </li>
-            <li className="orbit-slide">
-              <img className="orbit-image" src="/img/corgie2.jpg" alt="Space"/>
-              <figcaption className="orbit-caption">Chompers, THe Corgie!!!.</figcaption>
-            </li>
-            <li className="orbit-slide">
-              <img className="orbit-image" src="/img/corgie3.jpg" alt="Space"/>
-              <figcaption className="orbit-caption">Chompers, THe Corgie!!!.</figcaption>
-            </li>
-            <li className="orbit-slide">
-              <img className="orbit-image" src="/img/corgie4.jpg" alt="Space"/>
-              <figcaption className="orbit-caption">Chompers, THe Corgie!!!.</figcaption>
-            </li>
-          </ul>
-        </div>
+        <h3>Slides Component</h3>
       </div>
     )
   }
